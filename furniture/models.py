c74e4d1c455abd20f4425ff1479d92f291f0bb50
Giga Amiridze <gigaamiridze@gmail.com>
2023-09-15 11:05:19 +0400

    Added Product model

--- furniture/models.py
+++ furniture/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.core.validators import MinLengthValidator, MaxLengthValidator
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    supplier = models.CharField(max_length=100, null=False, blank=False)
+    image_url = models.CharField(max_length=300, null=False, blank=False)
+    price = models.DecimalField(max_digits=7, decimal_places=2, null=False, blank=False)
+    rating = models.DecimalField(max_digits=3, decimal_places=1, null=False, blank=False)
+    quantity = models.IntegerField(default=0, null=False, blank=False)
+    description = models.TextField(validators=[MinLengthValidator(10), MaxLengthValidator(300)], null=False, blank=False)
+    location = models.CharField(max_length=100, null=False, blank=False)
