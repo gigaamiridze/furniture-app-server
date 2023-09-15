cd48ac3f9549d9b3faaae7a1df3d3be82370ec33
Giga Amiridze <gigaamiridze@gmail.com>
2023-09-15 10:36:14 +0400

    Installed furniture app, created urls file inside the furniture folder and added get routes view and registered it

--- app/settings.py
+++ app/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'furniture.apps.FurnitureConfig',
 ]
 
 MIDDLEWARE = [
--- furniture/views.py
+++ furniture/views.py
@@ -1,3 +1,39 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+@api_view(['GET'])
+def get_routes(request):
+    routes = [
+        {
+            'Endpoint': '/products',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns an array of products'
+        },
+        {
+            'Endpoint': '/products/id',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns a single product object'
+        },
+        {
+            'Endpoint': '/products',
+            'method': 'POST',
+            'body': {'body': ''},
+            'description': 'Creates new product with data sent in post request'
+        },
+        {
+            'Endpoint': '/products/id',
+            'method': 'PUT',
+            'body': {'body': ''},
+            'description': 'Creates an existing product with data sent in post request'
+        },
+        {
+            'Endpoint': '/products/id',
+            'method': 'DELETE',
+            'body': None,
+            'description': 'Deletes and exiting product'
+        },
+    ]
+
+    return Response(routes)
