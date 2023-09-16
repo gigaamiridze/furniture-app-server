c96f1078f734182680492e41c44c84969add703d
Giga Amiridze <gigaamiridze@gmail.com>
2023-09-16 10:00:35 +0400

    Added location model and one-to-many relationship between product and location models

--- furniture/admin.py
+++ furniture/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, Location
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ('name', 'price', 'quantity', 'location',)
     list_filter = ('name', 'supplier', 'price', 'rating', 'quantity', 'location',)
 
 admin.site.register(Product, ProductAdmin)
+admin.site.register(Location)
--- furniture/models.py
+++ furniture/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from django.core.validators import MinLengthValidator, MaxLengthValidator
 
+class Location(models.Model):
+    country = models.CharField(max_length=100, null=False, blank=False)
+    city = models.CharField(max_length=100, null=False, blank=False)
+
+    def __str__(self):
+        return f'{self.country}, {self.city}'
+
 class Product(models.Model):
     name = models.CharField(max_length=100, null=False, blank=False)
     supplier = models.CharField(max_length=100, null=False, blank=False)
@@ -9,7 +16,7 @@ class Product(models.Model):
     rating = models.DecimalField(max_digits=3, decimal_places=1, null=False, blank=False)
     quantity = models.IntegerField(default=0, null=False, blank=False)
     description = models.TextField(validators=[MinLengthValidator(10), MaxLengthValidator(300)], null=False, blank=False)
-    location = models.CharField(max_length=100, null=False, blank=False)
+    location = models.ForeignKey(Location, on_delete=models.CASCADE, null=False, related_name='products')
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
