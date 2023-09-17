04604a1a230bf75a6590e86e232bf6854fd87d6e
Giga Amiridze <gigaamiridze@gmail.com>
2023-09-17 09:53:52 +0400

    Created location serializer and added it in product serializer as a field

--- furniture/serializers.py
+++ furniture/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework.serializers import ModelSerializer
-from .models import Product
+from .models import Product, Location
+
+class LocationSerializer(ModelSerializer):
+    class Meta:
+        model = Location
+        fields = ['country', 'city']
 
 class ProductSerializer(ModelSerializer):
+    location = LocationSerializer()
+
     class Meta:
         model = Product
         fields = '__all__'
