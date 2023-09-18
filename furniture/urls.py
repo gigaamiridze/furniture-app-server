dcf52cd64aeb9327b0a6273624dafecb3af68e4b
Giga Amiridze <gigaamiridze@gmail.com>
2023-09-18 09:02:56 +0400

    Added search product view functionality

--- furniture/urls.py
+++ furniture/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.get_routes, name='routes'),
     path('products', views.get_products, name='products'),
-    path('products/<str:pk>', views.get_product, name='product'),
+    path('products/<int:pk>', views.get_product, name='product'),
     path('create-product', views.create_product, name='create_product'),
+    path('search/<str:key>', views.search_product, name='search_product'),
 ]
--- furniture/views.py
+++ furniture/views.py
@@ -15,7 +15,7 @@ def get_routes(request):
             'description': 'Returns an array of products'
         },
         {
-            'Endpoint': '/products/id',
+            'Endpoint': '/products/<int:pk>',
             'method': 'GET',
             'body': None,
             'description': 'Returns a single product object'
@@ -25,6 +25,12 @@ def get_routes(request):
             'method': 'POST',
             'body': {'body': ''},
             'description': 'Creates new product with data sent in post request'
+        },
+        {
+            'Endpoint': '/search/<str:key>',
+            'method': 'GET',
+            'body': None,
+            'description': 'Returns a single product object or an array of products'
         },
         {
             'Endpoint': '/products/id',
@@ -55,12 +61,24 @@ def get_product(request, pk):
         serializer = ProductSerializer(product, many=False)
         return Response(serializer.data, status=status.HTTP_200_OK)
     except ObjectDoesNotExist:
-        return Response(f'Product with ID {pk} not found', status=status.HTTP_404_NOT_FOUND)
+        return Response({'error': f'Product with ID {pk} not found.'}, status=status.HTTP_404_NOT_FOUND)
 
 @api_view(['POST'])
 def create_product(request):
     serializer = ProductSerializer(data=request.data)
+
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def search_product(request, key):
+    products = Product.objects.filter(name__icontains=key)
+
+    if not products:
+        return Response({'error': f'No products found for the search key "{key}".'}, status=status.HTTP_404_NOT_FOUND)
+
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data, status=status.HTTP_200_OK)
